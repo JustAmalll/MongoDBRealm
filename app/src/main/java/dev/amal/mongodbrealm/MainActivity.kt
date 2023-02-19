409308acd11e271f286a828953005a10ede7c651
JustAmalll <dev.justamal@gmail.com>
2023-02-19 12:42:51 +0600

    dagger hilt setup

--- app/src/main/java/dev/amal/mongodbrealm/MainActivity.kt
+++ app/src/main/java/dev/amal/mongodbrealm/MainActivity.kt
@@ -3,8 +3,10 @@ package dev.amal.mongodbrealm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 import dev.amal.mongodbrealm.ui.theme.MongoDBRealmTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/dev/amal/mongodbrealm/MyApplication.kt
@@ -0,0 +1,7 @@
+package dev.amal.mongodbrealm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application()
