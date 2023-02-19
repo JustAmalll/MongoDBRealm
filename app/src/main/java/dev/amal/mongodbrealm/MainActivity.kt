f34a2f40c8047de6247687cefd39c81104ac8a9f
JustAmalll <dev.justamal@gmail.com>
2023-02-19 13:10:12 +0600

    HomeScreen setup / Project Done!

--- app/src/main/java/dev/amal/mongodbrealm/MainActivity.kt
+++ app/src/main/java/dev/amal/mongodbrealm/MainActivity.kt
@@ -3,7 +3,10 @@ package dev.amal.mongodbrealm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dev.amal.mongodbrealm.screen.HomeScreen
+import dev.amal.mongodbrealm.screen.HomeViewModel
 import dev.amal.mongodbrealm.ui.theme.MongoDBRealmTheme
 
 @AndroidEntryPoint
@@ -12,7 +15,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MongoDBRealmTheme {
-
+                val viewModel: HomeViewModel = hiltViewModel()
+                val data = viewModel.data
+                HomeScreen(
+                    data = data,
+                    filtered = viewModel.filtered,
+                    name = viewModel.name,
+                    objectId = viewModel.objectId,
+                    onNameChanged = { viewModel.updateName(name = it) },
+                    onObjectIdChanged = { viewModel.updateObjectId(id = it) },
+                    onInsertClicked = { viewModel.insertPerson() },
+                    onUpdateClicked = { viewModel.updatePerson() },
+                    onDeleteClicked = { viewModel.deletePerson() },
+                    onFilterClicked = { viewModel.filterData() }
+                )
             }
         }
     }
